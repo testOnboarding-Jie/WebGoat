aca504e88a8cc2cfb64887010b91f5a972dfe9d4
Jie Huang <huangj@blackduck.com>
2025-11-27 10:23:18 -0700

    Fix format

--- src/main/java/org/owasp/webgoat/lessons/sqlinjection/introduction/SqlInjectionLesson5.java
+++ src/main/java/org/owasp/webgoat/lessons/sqlinjection/introduction/SqlInjectionLesson5.java
@@ -48,6 +48,7 @@ public class SqlInjectionLesson5 extends AssignmentEndpoint {
   private final LessonDataSource dataSource;
   // ...existing code...
   private static final String DB_PASSWORD = "SuperSecretPassword123!"; // Hardcoded credential
+
   // ...existing code...
 
   public SqlInjectionLesson5(LessonDataSource dataSource) {
